08f0f70b22eef0c0e0d845b7f78f458c661d47a2
ssompal1 <shravya_sompalli@brown.edu>
2023-06-30 00:29:20 +0200

    feat:changed to rgb color scheme

--- src/script.js
+++ src/script.js
@@ -50,6 +50,7 @@ paint: {
 3,
 5
 ],
+/*
 'heatmap-color': [
     'interpolate',
     ['linear'],
@@ -66,7 +67,24 @@ paint: {
     'rgb(155,155,255)',
     1,
     'rgb(180,180,255)'
-    ]},
+    ]},*/
+    'heatmap-color': [
+        'interpolate',
+        ['linear'],
+        ['heatmap-density'],
+        0,
+        'rgba(0,0,100,0)',
+        0.2,
+        'rgb(21,101,192)',
+        0.4,
+        'rgb(25,118,210)',
+        0.6,
+        'rgb(33,150,243)',
+        0.8,
+        'rgb(100,181,246)',
+        1,
+        'rgb(144,202,250)'
+        ]},
 /*
 'heatmap-color': [
     'interpolate',
@@ -106,7 +124,7 @@ map.addLayer({
     'interpolate',
     ['linear'],
     ['zoom'],
-    0,
+    0.05,
     0.3,
     0.5,
     1
@@ -144,6 +162,7 @@ map.addLayer({
         ]},*/
         //ORANGE
      //RED  
+     /*
      'heatmap-color': [
         'interpolate',
         ['linear'],
@@ -160,7 +179,25 @@ map.addLayer({
         'rgb(229,101,101)',
         1,
         'rgb(239,181,181)'
-        ]},
+        ]},*/
+        //TREE GREEN
+        'heatmap-color': [
+            'interpolate',
+            ['linear'],
+            ['heatmap-density'],
+            0,
+            'rgba(102,0,0,0)',
+            0.2,
+            'rgb(60,100,0)',
+            0.4,
+            'rgb(131,220,0)',
+            0.6,
+            'rgb(166,255,35)',
+            0.8,
+            'rgb(198,255,115)',
+            1,
+            'rgb(219,255,165)'
+            ]},
 
     'filter': ['all', filterYear, filterU]
     });
@@ -200,22 +237,39 @@ map.addLayer({
     5
     ], 
     //LIGHT BLUE
+    // 'heatmap-color': [
+    //     'interpolate',
+    //     ['linear'],
+    //     ['heatmap-density'],
+    //     0,
+    //     'rgba(0,72,100,0)',
+    //     0.2,
+    //     'rgb(0,76,160)',
+    //     0.4,
+    //     'rgb(5,124,255)',
+    //     0.6,
+    //     'rgb(55,206,255)',
+    //     0.8,
+    //     'rgb(165,182,255)',
+    //     1,
+    //     'rgb(165,208,255)'
+    //     ]},
     'heatmap-color': [
         'interpolate',
         ['linear'],
         ['heatmap-density'],
         0,
-        'rgba(0,72,100,0)',
+        'rgba(160,0,95,0)',
         0.2,
-        'rgb(0,76,160)',
+        'rgb(160,0,95)',
         0.4,
-        'rgb(5,124,255)',
+        'rgb(255,5,154)',
         0.6,
-        'rgb(55,206,255)',
+        'rgb(255,85,186)',
         0.8,
-        'rgb(165,182,255)',
+        'rgb(255,145,210)',
         1,
-        'rgb(165,208,255)'
+        'rgb(255,205,235)'
         ]},
     layout: {
     visibility: 'none'},
