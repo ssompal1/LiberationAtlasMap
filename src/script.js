674b0ec2c590aab4b8d52e0655cf109ff2778200
ssompal1 <shravya_sompalli@brown.edu>
2023-12-05 23:48:12 -0500

    revision wo slider and with markers with panels

--- src/script.js
+++ src/script.js
@@ -5,10 +5,36 @@ style: 'mapbox://styles/mapbox/dark-v11',
 center: [-74.0059, 40.7128],
 zoom: 11
 });
- 
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-77.032, 38.913]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Washington, D.C.'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-122.414, 37.776]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'San Francisco, California'
+        }
+      }
+    ]
+  };
 map.on('load', () => {
-let filterYear = ['==', ['number', ['get', 'ARREST_YEAR']], 2000];
-let filterHouseYear = ['==', ['number', ['get', 'year_of_violation']], 2000];
+// let filterYear = ['==', ['number', ['get', 'ARREST_YEAR']], 2000];
+// let filterHouseYear = ['==', ['number', ['get', 'year_of_violation']], 2000];
 let filterP = ['==', ['string', ['get', 'LAW_CODE']], "PL 2300000"]
 let filterU = ['==', ['string', ['get', 'LAW_CODE']], "ED 6512001"]
 let filterDOB = ['==', ['string', ['get', 'type_of_violation']], "DOB code violation"]
@@ -104,7 +130,7 @@ paint: {
     'rgb(229,205,255)'
     ]},*/
 
-'filter': ['all', filterYear, filterP]
+'filter': ['all', filterP]
 });
 
 map.addLayer({
@@ -199,7 +225,7 @@ map.addLayer({
             'rgb(219,255,165)'
             ]},
 
-    'filter': ['all', filterYear, filterU]
+    'filter': ['all', filterU]
     });
 
 map.addLayer({
@@ -274,56 +300,56 @@ map.addLayer({
     layout: {
     visibility: 'none'},
     
-    'filter': ['all', filterHouseYear]
+    'filter': ['all']
     });
 // update hour filter when the slider is dragged
-document.getElementById('slider').addEventListener('input', (event) => {
-    const year = parseInt(event.target.value);
-    //update the map
-    const cumBreakdown = document.getElementById("cumulative").checked
-    const byYearBreakdown = document.getElementById("byYear").checked
-    if (cumBreakdown) {
-        filterYear = ['<=', ['number', ['get', 'ARREST_YEAR']], year];
-        filterHouseYear = ['<=', ['number', ['get', 'Year']], year];
-    }
-    else if (byYearBreakdown) {
-        filterYear = ['==', ['number', ['get', 'ARREST_YEAR']], year];
-        filterHouseYear = ['==', ['number', ['get', 'Year']], year];
-    }
-    else {
-        console.log('error');
-    }  
-    filterP = ['==', ['string', ['get', 'LAW_CODE']], "PL 2300000"]
-    filterU = ['==', ['string', ['get', 'LAW_CODE']], "ED 6512001"]
-    map.setFilter('collisions', ['all', filterYear, filterP]);
-    map.setFilter('collisions1', ['all', filterYear, filterU]);
-    map.setFilter('collisions2',['all',filterHouseYear]);
-    // update text in the UI
-    document.getElementById('active-year').innerText = year;
-    });
+// document.getElementById('slider').addEventListener('input', (event) => {
+//     const year = parseInt(event.target.value);
+//     //update the map
+//     const cumBreakdown = document.getElementById("cumulative").checked
+//     const byYearBreakdown = document.getElementById("byYear").checked
+//     if (cumBreakdown) {
+//         filterYear = ['<=', ['number', ['get', 'ARREST_YEAR']], year];
+//         filterHouseYear = ['<=', ['number', ['get', 'Year']], year];
+//     }
+//     else if (byYearBreakdown) {
+//         filterYear = ['==', ['number', ['get', 'ARREST_YEAR']], year];
+//         filterHouseYear = ['==', ['number', ['get', 'Year']], year];
+//     }
+//     else {
+//         console.log('error');
+//     }  
+//     filterP = ['==', ['string', ['get', 'LAW_CODE']], "PL 2300000"]
+//     filterU = ['==', ['string', ['get', 'LAW_CODE']], "ED 6512001"]
+//     map.setFilter('collisions', ['all', filterYear, filterP]);
+//     map.setFilter('collisions1', ['all', filterYear, filterU]);
+//     map.setFilter('collisions2',['all',filterHouseYear]);
+//     // update text in the UI
+//     document.getElementById('active-year').innerText = year;
+//     });
     
 //update map when toggle is changed
-document
-.getElementById('timefilters')
-.addEventListener('input', (e) => {
-    const timeBreakdown = e.target.value;
-    const year = parseInt(document.getElementById('active-year').innerText)
-    console.log(timeBreakdown)
-    if (timeBreakdown == 'cumulative') {
-        filterYear = ['<=', ['number', ['get', 'ARREST_YEAR']], year];
-        filterHouseYear = ['<=', ['number', ['get', 'Year']], year];
-    }
-    else if (timeBreakdown == 'byYear') {
-        filterYear = ['==', ['number', ['get', 'ARREST_YEAR']], year];
-        filterHouseYear = ['==', ['number', ['get', 'Year']], year];
-    }
-    else {
-        console.log('error');
-    }
-    map.setFilter('collisions', ['all', filterYear, filterP]);
-    map.setFilter('collisions1', ['all', filterYear, filterU]);
-    map.setFilter('collisions2',['all',filterHouseYear]);
-});
+// document
+// .getElementById('timefilters')
+// .addEventListener('input', (e) => {
+//     const timeBreakdown = e.target.value;
+//     const year = parseInt(document.getElementById('active-year').innerText)
+//     console.log(timeBreakdown)
+//     if (timeBreakdown == 'cumulative') {
+//         filterYear = ['<=', ['number', ['get', 'ARREST_YEAR']], year];
+//         filterHouseYear = ['<=', ['number', ['get', 'Year']], year];
+//     }
+//     else if (timeBreakdown == 'byYear') {
+//         filterYear = ['==', ['number', ['get', 'ARREST_YEAR']], year];
+//         filterHouseYear = ['==', ['number', ['get', 'Year']], year];
+//     }
+//     else {
+//         console.log('error');
+//     }
+//     map.setFilter('collisions', ['all', filterYear, filterP]);
+//     map.setFilter('collisions1', ['all', filterYear, filterU]);
+//     map.setFilter('collisions2',['all',filterHouseYear]);
+// });
 
 
 
@@ -342,6 +368,103 @@ for (let i=0;i<checks.length;i++){
         });
 
 }
+
+// Add a marker to the map
+});
+for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = document.createElement('div');
+    el.className = 'marker';
+  
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+  }
+
+const e1 = document.createElement('div');
+e1.className = 'marker';
+
+const marker1 = new mapboxgl.Marker(e1)
+.setLngLat([-73.8831, 40.7557])
+.addTo(map);
+
+const e2 = document.createElement('div');
+e2.className = 'marker';
+const marker2 = new mapboxgl.Marker(e2)
+.setLngLat([-74.00,40.71])
+.addTo(map);
+const e3 = document.createElement('div');
+e3.className = 'marker';
+const marker3 = new mapboxgl.Marker(e3)
+.setLngLat([-73.9757, 40.7479])
+.addTo(map);
+const e4 = document.createElement('div');
+e4.className = 'marker';
+const marker4 = new mapboxgl.Marker(e4)
+.setLngLat([-73.9970, 40.7158])
+.addTo(map);
+const e5 = document.createElement('div');
+e5.className = 'marker';
+const marker5 = new mapboxgl.Marker(e5)
+.setLngLat([-73.8307, 40.7685])
+.addTo(map);
+
+
+
+// Add a popup to the marker
+// var popup = new mapboxgl.Popup({ closeButton: false })
+//     .setHTML('<h3>Marker Title</h3><p>Marker description.</p>');
+
+// marker2.setPopup(popup);
+
+// // Add a click event to the marker
+marker1.getElement().addEventListener('click', function() {
+    // Show the panel
+    console.log("Clicked");
+    document.getElementById('popup-title').innerText = 'NAME';
+    document.getElementById('popup-description').innerText = 'Person 1';
+    document.getElementById('interview-title').innerText = 'DESCRIPTION'
+    document.getElementById('interview-description').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    document.getElementById('panel').style.display = 'block';
+});
+marker2.getElement().addEventListener('click', function() {
+    // Show the panel
+    console.log("Clicked");
+    document.getElementById('popup-title').innerText = 'NAME';
+    document.getElementById('popup-description').innerText = 'Person 2';
+    document.getElementById('interview-title').innerText = 'DESCRIPTION'
+    document.getElementById('interview-description').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    document.getElementById('panel').style.display = 'block';
+});
+marker3.getElement().addEventListener('click', function() {
+    // Show the panel
+    console.log("Clicked");
+    document.getElementById('popup-title').innerText = 'NAME';
+    document.getElementById('popup-description').innerText = 'Person 3';
+    document.getElementById('interview-title').innerText = 'DESCRIPTION'
+    document.getElementById('interview-description').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    document.getElementById('panel').style.display = 'block';
+});
+marker4.getElement().addEventListener('click', function() {
+    // Show the panel
+    console.log("Clicked");
+    document.getElementById('popup-title').innerText = 'NAME';
+    document.getElementById('popup-description').innerText = 'Anonymous';
+    document.getElementById('interview-title').innerText = 'DESCRIPTION'
+    document.getElementById('interview-description').innerText = ' "At first, I was in it for money only. I started because I needed money. I believed it was worthwhile to sacrifice myself for my family and my siblings. I would have done anything if I could earn money… I was ready to sacrifice myself, and now it’s my job. Now… I think the world needs this kind of occupation and we need people who do this work for others… in our society. So now, rather than considering my job as excruciating, I think that massage work is an occupation that contributes to the development of the society, like a wheel of a wheelbarrow. Other people might think I’m crazy or I should say such a thing. But in my opinion, massage work is an indispensable occupation. I think someone has to do this work, because everyone needs massage." '
+    document.getElementById('panel').style.display = 'block';
+});
+marker5.getElement().addEventListener('click', function() {
+    // Show the panel
+    console.log("Clicked");
+    document.getElementById('popup-title').innerText = 'NAME';
+    document.getElementById('popup-description').innerText = 'Person 5';
+    document.getElementById('interview-title').innerText = 'DESCRIPTION'
+    document.getElementById('interview-description').innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    document.getElementById('panel').style.display = 'block';
 });
 
+// // Close the panel when the map is clicked
+// map.on('click', function() {
+//     document.getElementById('panel').style.display = 'none';
+// });
 
