8e281ca74c8a6b668ef7d4cf746285a3fb796fa9
Shravya Sompalli <shravya_sompalli@brown.edu>
2025-01-05 00:47:44 -0500

    deleting toml and netlify function

--- netlify/functions/getMapboxKey.js
+++ netlify/functions/getMapboxKey.js
@@ -1,6 +1,6 @@
-exports.handler = async () => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ key: process.env.MAPBOX_API_KEY }),
-    };
-  };
+// exports.handler = async () => {
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ key: process.env.MAPBOX_API_KEY }),
+//     };
+//   };
