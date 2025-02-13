671165b15f1b33fdeca24ff4cd46eea6f7065aa9
Shravya Sompalli <shravyasompalli@mac.mynetworksettings.com>
2025-02-12 21:11:23 -0500

    delete key

--- public/src/script.js
+++ public/src/script.js
@@ -1,4 +1,3 @@
-// mapboxgl.accessToken = "pk.eyJ1Ijoic2hyYXZ5YS1zb21wYWxsaSIsImEiOiJjbGEycmZ3YnAwMzA4M3dwbjExejNqeDczIn0.U0UKRmRLgqJVkz1BizIQBg"
 fetch('/.netlify/functions/getMapboxKey')
   .then(response => {
     if (!response.ok) {
